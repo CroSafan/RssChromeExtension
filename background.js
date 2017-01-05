420d3c1e9eb8422232e6cc67ef21c8408689ebab
Antun Safaric <crosafan@gmail.com>
2017-01-05 22:58:35 +0100

    Code formatting

--- background.js
+++ background.js
@@ -1,64 +1,50 @@
 $(document).ready(function() {
 
-var notificationNumber=1;
-
-function getFeed(){
- chrome.browserAction.getBadgeText({}, function(result) {
-    if(result==="0")notificationNumber=1;
-});
-   storageOfCurrentFeed = new Array();
+   var notificationNumber=1;
    
-   chrome.browserAction.setBadgeBackgroundColor({color: "#CF0016"});
-   //https://www.mev.hr/index.php/category/racunarstvo-rss/racunarstvo/feed/
-   //feed to parse
-    const url = 'http://lorem-rss.herokuapp.com/feed?unit=minute';
-  
-  feednami.load(url)
-    .then(feed => {
+   function getFeed(){
+    chrome.browserAction.getBadgeText({}, function(result) {
+       if(result==="0")notificationNumber=1;
+   });
+      storageOfCurrentFeed = new Array();
       
-      console.log(feed);
+      chrome.browserAction.setBadgeBackgroundColor({color: "#CF0016"});
+      //https://www.mev.hr/index.php/category/racunarstvo-rss/racunarstvo/feed/
+      //feed to parse
+       const url = 'http://lorem-rss.herokuapp.com/feed?unit=minute';
      
-      
-      for(let entry of feed.entries){        
-     storageOfCurrentFeed.push(entry.guid);
-      }
-      chrome.storage.local.get("value", function(items) {   
-        // console.log(items);        
-        if(feed.entries[0].guid!==items.value[0]){
-              chrome.browserAction.setBadgeText({ text: String(notificationNumber)});
-                var opt = {
-               type: "basic",
-               title: feed.entries[0].title,
-               message: feed.entries[0].link,
-               iconUrl: "icon.png"};                            
-            chrome.notifications.create( opt);
-            notificationNumber+=1;
-        }
-    }); 
-      
-      
-      
-      
-      
-      
-      chrome.storage.local.set({'value': storageOfCurrentFeed}, function() {
-          // Notify that we saved.
-          console.log('Settings saved');
-        });
-      
-      
-      
-      
-      
-      
-      
-    });
-    
-        
-}
-
-setInterval(getFeed,10000);
-getFeed();
+     feednami.load(url)
+       .then(feed => {      
+         console.log(feed);     
+         
+            for(let entry of feed.entries){        
+               storageOfCurrentFeed.push(entry.guid);
+            }
+            //dohvacanje lokalnog spremista
+            chrome.storage.local.get("value", function(items) {   
+              //console.log(items);           
+              //provjera ako je obavijest nova
+              if(feed.entries[0].guid!==items.value[0]){
+                    chrome.browserAction.setBadgeText({ text: String(notificationNumber)});
+                      var opt = {
+                     type: "basic",
+                     title: feed.entries[0].title,
+                     message: feed.entries[0].link,
+                     iconUrl: "icon.png"};                            
+                  chrome.notifications.create( opt);
+                  notificationNumber+=1;
+              }
+          }); 
+            //spremanje obavijesti u lokalnu memoriju browsera
+            chrome.storage.local.set({'value': storageOfCurrentFeed}, function() {
+                // Notify that we saved.
+                console.log('Settings saved');
+              });
+       });        
+   }
+   //provjera svakih 10 sekundi ako postoji nova obavijest
+   setInterval(getFeed,10000);
+   getFeed();
 
 
 });
--- popup.js
+++ popup.js
@@ -1,53 +1,32 @@
 $(document).ready(function() {
-//feed to parse
-
 
 function getFeed(){
   const textarea = document.getElementById('rssfeed');
   textarea.innerHTML="";
-   storageOfCurrentFeed = new Array();
+  storageOfCurrentFeed = new Array();
   
-   chrome.browserAction.setBadgeBackgroundColor({color: "#CF0016"});
-   //https://www.mev.hr/index.php/category/racunarstvo-rss/racunarstvo/feed/
-const url = 'http://lorem-rss.herokuapp.com/feed?unit=minute';
+  chrome.browserAction.setBadgeBackgroundColor({color: "#CF0016"});
+  //https://www.mev.hr/index.php/category/racunarstvo-rss/racunarstvo/feed/
+  //feed to parse
+  const url = 'http://lorem-rss.herokuapp.com/feed?unit=minute';
   
   feednami.load(url)
     .then(feed => {
       textarea.value = '';
-      console.log(feed);
-      
+      console.log(feed);      
       
       for(let entry of feed.entries){
         var content="";
-        content +="<div class='rrs-item' style='margin: 0 auto; width: 450px; height: auto;border: solid 1px black;display:block;text-overflow:ellipsis;' >"+"<b>"+ entry.title+"</b>";
-      content+='<p><a href="'+entry.link+'">'+entry.link+'</a></p>'+"</div>";
-     storageOfCurrentFeed.push(entry.guid);  
-      textarea.innerHTML+=content;     
-      
-      
-      }
-      
+        content +="<div class='rss-item'>"+"<b>"+ entry.title+"</b>";
+        content+='<p><a href="'+entry.link+'">'+entry.link+'</a></p>'+"</div>";
+        storageOfCurrentFeed.push(entry.guid);  
+        textarea.innerHTML+=content;     
+      }      
       chrome.browserAction.setBadgeText({ text: String(0)});
-      
-      
-      
-      
-      
-      
-      
-      
-      
-      
-      
-      
-      
-    });
-    
+    });   
         
 }
 
-
 getFeed();
-
 
 });
